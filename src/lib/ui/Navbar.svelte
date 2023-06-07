32ef2a06bdd919ce7437dc6e2b2388374a7678c5
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-06-07 12:20:46 +0200

    Working on Navbar

--- src/lib/ui/Navbar.svelte
+++ src/lib/ui/Navbar.svelte
@@ -3,6 +3,6 @@
 </script>
 <div class="w-[100%] h-[100%] bg-black flex overflow-hidden">
     <img src="src/lib/logo/OnlyLogo.png"  alt="Logo" class="rounded-full" />
-    <div class=" flex items-center text-3xl text-red-700">Gym Manger</div>
-    <input type="button" class="text-red-700 absolute right-0 text-2xl bg-blue-50 " value="Login" />
+    <div class=" flex items-center text-3xl text-red-700 ">Gym Manager</div>
+    <div class=" h-full flex  text-2xl w-full justify-end  items-center"><button class="text-red-700 h-[50%]  bg-blue-950 p-3 mr-10 " >Login</button> </div>
 </div>
