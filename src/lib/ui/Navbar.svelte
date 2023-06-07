2dbdbca133795d91d2749811b1146d8be37881a6
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-06-07 10:44:48 +0200

    Logo and Ui

---
+++ src/lib/ui/Navbar.svelte
@@ -0,0 +1,4 @@
+<script>
+
+</script>
+<div class="w-[100%] h-[100%] bg-secondary-900 flex"><img src="../logo/OnlyLogo.png" alt="Logo"> </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
+<script>
+	import Navbar from "$lib/ui/Navbar.svelte";
+</script>
+
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-<div class="w-[100vw] h-[5vh]">HEllo</div>
+<div class="w-[100vw] h-[10vh]">
+	<Navbar ></Navbar>
+</div>
 <div class="w-[100vw] h-[90vh] overflow-scroll">
 	<div class="w-[100%] h-[100%] bg-amber-200"></div>
 	<div class="w-[100%] h-[100%] bg-amber-100"></div>
