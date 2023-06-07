5e6ca736c746144c01f722bcfe85178f4f89c63b
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-06-07 11:11:35 +0200

    UI managment

--- src/lib/ui/Navbar.svelte
+++ src/lib/ui/Navbar.svelte
@@ -1,4 +1,8 @@
 <script>
 
 </script>
-<div class="w-[100%] h-[100%] bg-secondary-900 flex"><img src="../logo/OnlyLogo.png" alt="Logo"> </div>
+<div class="w-[100%] h-[100%] bg-black flex overflow-hidden">
+    <img src="src/lib/logo/OnlyLogo.png"  alt="Logo" class="rounded-full" />
+    <div class=" flex items-center text-3xl text-red-700">Gym Manger</div>
+    <input type="button" class="text-red-700 absolute right-0 text-2xl bg-blue-50 " value="Login" />
+</div>
