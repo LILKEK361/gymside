5180e5f98fbe218fdb69f56b9df0cf4788acccd9
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-06-08 07:33:04 +0200

    Desiging the exercise cards and db plan

---
+++ src/lib/ui/UebungCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+    export let exercisename;
+    export let Muskelgroup;
+    export let doing;
+
+
+</script>
+
+
+
+
+<div class="card  w-[20vw] bg-base-100 shadow-xl">
+    <figure class="px-10 pt-10">
+        <img src="src/lib/logo/FullLogo.png" alt="Shoes" class="rounded-xl" />
+    </figure>
+    <div class="card-body ">
+        <h2 class="card-title">Exercise: {exercisename}</h2>
+        <p>Muscle group: {Muskelgroup}</p>
+        <p>Execution: {doing}</p>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Navbar from "$lib/ui/Navbar.svelte";
+	import UebungCard from "$lib/ui/UebungCard.svelte";
 </script>
 
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
@@ -7,6 +8,8 @@
 	<Navbar ></Navbar>
 </div>
 <div class="w-[100vw] h-[90vh] overflow-scroll">
-	<div class="w-[100%] h-[100%] bg-amber-200"></div>
+	<div class="w-[100%] h-[100%] bg-amber-200">
+		<UebungCard exercisename="Curls" Muskelgroup="Arms" doing="Get Hantels and start moving"></UebungCard>
+	</div>
 	<div class="w-[100%] h-[100%] bg-amber-100"></div>
 </div>
