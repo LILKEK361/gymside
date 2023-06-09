854b67b3ba28853b59f5489e5f1ea0d031f42772
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-06-09 08:34:17 +0200

    Working on the ui

--- src/lib/ui/WelcomeText.svelte
+++ src/lib/ui/WelcomeText.svelte
@@ -5,14 +5,19 @@
 <div class="w-full h-full flex overflow-hidden" style="background-image: url('src/lib/logo/img_1.png'); background-repeat: no-repeat; background-size: cover;
 
  ">
-    <div class="h-[100%] w-[50%]  flex justify-center items-center">
-        <img src="src/lib/logo/FullLogo.png" alt="Full Logo" class="w-[65%] h-[30%] " >
+    <div class="h-[100%] w-[50%]  flex justify-center items-center ">
+        <img src="src/lib/logo/FullLogo.png" alt="Full Logo" class="w-[65%] h-[30%]  bg-transparent/70  border-4  border-red-600 " >
     </div>
-    <div class="h-[100%] w-[50%]  text-3xl text-center">
-        <p>Featrues:</p>
-        <p>Create your own Workouts</p>
-        <p> Manage your own Workouts</p>
-        <p>Add your own Excer</p>
+    <div class="h-[100%] w-[50%]  text-3xl flex justify-center items-center ">
+        <div class=" bg-transparent/70 p-16 ">
+            <p >Featrues:</p>
+            <br>
+            <p class="hover:text-red-700">> Create your own Workouts</p>
+            <br>
+            <p class="hover:text-red-700">> Manage your own Workouts</p>
+            <br>
+            <p class="hover:text-red-700">> Add your own Excer</p>
+        </div>
     </div>
 
 </div>
