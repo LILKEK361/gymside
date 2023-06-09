2d22dd5936c1c2925db279ceba94dd5b24741970
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-06-09 11:52:07 +0200

    Update Navbar.svelte

--- src/lib/ui/Navbar.svelte
+++ src/lib/ui/Navbar.svelte
@@ -4,7 +4,23 @@ import {t}from "/src/lib/Firebase/firebase.ts"
 <div class="w-[100%] h-[100%] bg-gray-950 flex overflow-hidden">
     <img src="src/lib/logo/OnlyLogo.png"  alt="Logo" class="rounded-full" />
     <div class=" flex items-center text-3xl text-red-700 ">Gym Manager</div>
-    <div class="w-full flex justify-end items-center "><button class="btn btn-square btn-outline mr-10 ">
+    <div class="w-full flex justify-end items-center "><button class="btn" onclick="LoginSignup.showModal()">
         {"Login"}
-    </button></div>
+    </button>
+        <dialog id="LoginSignup" class="modal">
+            <form method="dialog" class="modal-box text-center">
+                <h3 class="font-bold text-lg ">Login / Sign Up</h3>
+                <p>Google</p>
+                <p>GitHub</p>
+
+                <p class="py-4">Press ESC key or click outside to close</p>
+            </form>
+            <form method="dialog" class="modal-backdrop">
+                <button>cancel</button>
+            </form>
+        </dialog>
+    </div>
+
+
+
 </div>
