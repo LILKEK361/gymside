a1061a124f8bce37a9ac888d8e48fb2b435dbd00
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-06-09 07:48:42 +0200

    background imgs for website

--- src/lib/ui/WelcomeText.svelte
+++ src/lib/ui/WelcomeText.svelte
@@ -2,14 +2,16 @@
     import MdAssignmentTurnedIn from 'svelte-icons/md/MdAssignmentTurnedIn.svelte'
 </script>
 
-<div class="w-full h-full flex overflow-hidden">
-    <div class="h-[100%] w-[50%] bg-gray-900 flex justify-center items-center">
+<div class="w-full h-full flex overflow-hidden" style="background-image: url('src/lib/logo/img_1.png'); background-repeat: no-repeat; background-size: cover;
+
+ ">
+    <div class="h-[100%] w-[50%]  flex justify-center items-center">
         <img src="src/lib/logo/FullLogo.png" alt="Full Logo" class="w-[65%] h-[30%] " >
     </div>
-    <div class="h-[100%] w-[50%] bg-gray-600 text-3xl text-center">
+    <div class="h-[100%] w-[50%]  text-3xl text-center">
         <p>Featrues:</p>
         <p>Create your own Workouts</p>
-        <p><MdAssignmentTurnedIn  /> Manage your own Workouts</p>
+        <p> Manage your own Workouts</p>
         <p>Add your own Excer</p>
     </div>
 
