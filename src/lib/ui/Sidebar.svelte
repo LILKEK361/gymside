084ad40cabdb5ea4f00447f162a05dc22e442148
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-06-12 09:39:34 +0200

    starting with userpage

--- src/lib/ui/Sidebar.svelte
+++ src/lib/ui/Sidebar.svelte
@@ -3,5 +3,20 @@
 </script>
 
 <div class="w-[100%] h-[100%] ">
-Sidebar {userid}
+    <div class="drawer">
+        <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content">
+            <!-- Page content here -->
+            <label for="my-drawer" class="btn btn-primary drawer-button">Open drawer</label>
+        </div>
+        <div class="drawer-side">
+            <label for="my-drawer" class="drawer-overlay"></label>
+            <ul class="menu p-4 w-80 h-full bg-base-200 text-base-content">
+                <!-- Sidebar content here -->
+                <li><a>Sidebar Item 1</a></li>
+                <li><a>Sidebar Item 2</a></li>
+
+            </ul>y
+        </div>
+    </div>
 </div>
