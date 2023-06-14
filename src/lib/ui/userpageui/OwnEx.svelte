1d291a014335789ee2d501e60047f2f84894eda1
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-06-14 16:29:41 +0200

    Startet with ownExPage design

--- src/lib/ui/userpageui/OwnEx.svelte
+++ src/lib/ui/userpageui/OwnEx.svelte
@@ -5,13 +5,14 @@
 
     import UebungCard from "$lib/ui/UebungCard.svelte";
     import {getOwnEx} from "$lib/Firebase/firebase";
+    import UebungsCardRow from "$lib/ui/userpageui/UebungsCardRow.svelte";
     let names;
     let ex;
 
     onMount( async () =>{
         await getOwnEx()
-        names   = localStorage.getItem("namesEx")?.split(",")
-        ex      = JSON.parse(localStorage.getItem("ownEx"))
+        names   = await localStorage.getItem("namesEx")?.split(",")
+        ex      = await JSON.parse(localStorage.getItem("ownEx"))
 
     })
 
@@ -21,14 +22,7 @@
 
 {#if names && ex}
     <div class="w-[100%] h-[100%] bg-green-700 overflow-hidden flex justify-center items-center">
-        {#each names as name }
-            <UebungCard
-                    exercisename={name}
-                    doing={ex[name].way}
-                    Muskelgroup={ex[name].muscelgroup}
-                    level={ex[name].level}
-            />
-        {/each}
+        <UebungsCardRow names={[names[0], names[1], names[2], names[3]]} ex={ex}></UebungsCardRow>
     </div>
     {:else}
     <h1>You didnt create any Exercises</h1>
---
+++ src/lib/ui/userpageui/UebungsCardRow.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+    import UebungCard from "$lib/ui/UebungCard.svelte";
+
+    export const names = []
+    export const ex = {}
+</script>
+
+<div class="w-[100%] h-[30%] flex justify-center items-center">
+    {#each names as name}
+    <UebungCard
+            exercisename={name}
+            doing={ex.name.way}
+            level={ex.name.level}
+            Muskelgroup={ex.names.muscelgroup} />
+    {/each}
+</div>
