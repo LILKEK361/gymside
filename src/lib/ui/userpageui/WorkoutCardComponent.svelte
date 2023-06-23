3764983180ceedd17e3199711e32d86b0eba42fd
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-06-23 07:21:36 +0200

    Created WorkoutCardComponent need a design idea

---
+++ src/lib/ui/userpageui/WorkoutCardComponent.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let name : any
+    export let uebunegen : any
+    export let img : any
+    export let time : any
+
+
+</script>
+
+<div>
+    {uebunegen,img,time,name}
+</div>
