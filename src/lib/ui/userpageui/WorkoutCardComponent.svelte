9205496be6a52428e2c7ca1f1edbfeeb8b5c7153
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-06-27 07:28:05 +0200

    Starting to implement the Workouts

--- src/lib/ui/userpageui/WorkoutCardComponent.svelte
+++ src/lib/ui/userpageui/WorkoutCardComponent.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-    export let name : any
-    export let uebunegen : any
-    export let img : any
+    export let name : string
+    //export let uebunegen : any
+    //export let img : any
     export let time : any
 
 
 </script>
 
 <div>
-    {uebunegen,img,time,name }
+    {time }
+    {name}
 </div>
--- src/lib/ui/userpageui/WorkoutComponent.svelte
+++ src/lib/ui/userpageui/WorkoutComponent.svelte
@@ -2,23 +2,24 @@
 
     import {onMount} from "svelte";
     import {getWorkouts} from "$lib/Firebase/firebase";
+    import WorkoutCardComponent from "$lib/ui/userpageui/WorkoutCardComponent.svelte";
 
     let names = [];
     onMount(async () => {
         let data = await getWorkouts();
 
         names = data.names;
-        console.log(names)
+
     })
 
 
 </script>
 
-<div class="w-[100%] h-[100%]">
+<div class="w-[100%] h-[100%] flex justify-center ">
 
     <ul>
         {#each names as name}
-            <li>{name}</li>
+           <li> <WorkoutCardComponent name={name} time="s" /></li>
         {/each}
     </ul>
 
