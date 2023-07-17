56205c68993d4a3924a564bb6cd47374fc8d515f
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-07-17 14:19:51 +0200

    Update loadData.ts

--- src/lib/ui/userpageui/loadData.ts
+++ src/lib/ui/userpageui/loadData.ts
@@ -18,6 +18,7 @@ export async function setAllData(){
 
 
                await data.legs.map((element : any) => {
+                   
                     allDes.set(element.name, element.description)
                 })
                 await data.back.map((element : any) => {
