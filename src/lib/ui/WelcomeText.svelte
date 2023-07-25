b05c3b652218495e903d527659ee9db3118caafe
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-07-25 14:29:27 +0200

    Ui update

--- src/lib/ui/WelcomeText.svelte
+++ src/lib/ui/WelcomeText.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
+    //background-image: url('src/lib/logo/mainbg.png'); background-repeat: no-repeat; background-size: cover;
 </script>
 
-<div class="w-full h-full flex overflow-hidden" style="background-image: url('src/lib/logo/mainbg.png'); background-repeat: no-repeat; background-size: cover;
-
- ">
+<div class="w-[100%] h-[100%] flex  overflow-hidden items-center  " style="">
     <div class="h-[100%] w-[50%]  flex justify-center items-center ">
         <img src="src/lib/logo/FullLogo.png" alt="Full Logo" class="w-[65%] h-[30%]  bg-transparent/70  border-4  border-red-600 " >
     </div>
+    <div class="h-[80%] w-[1%] border-2 bg-white  " ></div>
     <div class="h-[100%] w-[50%]  text-3xl flex justify-center items-center ">
         <div class=" bg-transparent/70 p-16 ">
             <p >Featrues:</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import Navbar from "$lib/ui/Navbar.svelte";
 
 	import WelcomeText from "$lib/ui/WelcomeText.svelte";
-	import Impressum from "$lib/ui/Impressum.svelte";
+
 	import {onMount} from "svelte";
 
 onMount(() => {
@@ -18,17 +18,11 @@ onMount(() => {
 </svelte:head>
 
 <div class="w-[100vw] h-[100vh]" data-theme="dracula">
-	<div class="w-[100vw] h-[10vh]">
+	<div class="w-[100vw] h-[8vh]">
 		<Navbar/>
 	</div>
-	<div class="w-[100vw] h-[90vh] overflow-y-scroll">
-		<div class="w-[100%] h-[100%] ">
-			<WelcomeText/>
-		</div>
-		<div id="CreateWorkoutplan" class="w-[100%] h-[100%] bg-gray-500">Workoutplan</div>
 
-
-		<div id="QRcode" class="w-[100%] h-[100%] bg-gray-800">QRcode</div>
-		<div class="w-full h-[20vh] bg-black"><Impressum /></div>
+	<div class="w-[100vw] h-[92vh] overflow-hidden">
+		<WelcomeText/>
 	</div>
 </div>
