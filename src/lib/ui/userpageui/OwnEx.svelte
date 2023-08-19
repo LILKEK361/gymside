30ef29723d9e3bc6df416e08f053f766259c04fd
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-08-19 16:06:59 +0200

    Bug fixing

--- src/lib/ui/userpageui/OwnEx.svelte
+++ src/lib/ui/userpageui/OwnEx.svelte
@@ -7,21 +7,21 @@
     import {onMount} from "svelte";
     import {getOwnEx} from "$lib/Firebase/firebase";
     import Toast from "$lib/ui/Toast/Toast.svelte";
-    import {Writable, writable} from "svelte/store";
+
 
 
 
 
     let entries = {}
     let entrieskey : Array<string>
-    let temp : Array<Array<string>>
-    export let chunks : Writable<Array<Array<string>>> = writable()
+
+    let chunks : Array<Array<string>> = [[""]]
+
     onMount(  async () =>{
-        let tem = await setup()
-        chunks.set(tem )
+       await setup()
     })
 
-    export async function setup(){
+    const setup =  async () => {
         entries =  await getOwnEx()
         entrieskey = Object.keys(entries)
         let tempData : Array<Array<string>> = [[""]]
@@ -40,7 +40,7 @@
         }
         console.log(tempData)
 
-        return tempData
+        chunks = tempData
     }
 
 
@@ -53,8 +53,8 @@
     <div class="w-full h-full flex justify-center items-center bg-gray-800">
         <div class="w-[90%] h-[90%]  ">
             {#each chunks as row}
-            <UebungsCardRow chunck={row} setup={setup} />
-        {/each}
+                <UebungsCardRow chunck={row} setup={(setup)} />
+            {/each}
     </div>
 
 </div>
--- src/lib/ui/userpageui/UebungsCardRow.svelte
+++ src/lib/ui/userpageui/UebungsCardRow.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
 
-    import ExCard from "$lib/ui/userpageui/ExCard.svelte";
+
+    import {deleteEx} from "$lib/Firebase/firebase";
+    import {notifications} from "$lib/ui/Toast/notification";
 
     export let chunck : Array<Array<string>> = [[""]]
 
-    export let setup : any
+    export let setup
+    function destroy(name : string){
+        console.log(name && setup)
+        if(name && setup){
+            deleteEx(name)
+            setup()
+            notifications.success("Db updated", 2000)
+        }else{
+            notifications.danger("Error on delete ", 2000)
+        }
+    }
+
 
 </script>
 
@@ -13,7 +26,25 @@
 
 
         {#each chunck as s}
-            <ExCard name={s} setup={setup}></ExCard>
+            <div class="card">
+                <ul>
+                    <li>{s}</li>
+                    <li><button class="button-base-styles bg-gray-200" on:click={destroy(s)}>Delete</button></li>
+                </ul>
+
+
+            </div>
         {/each}
 
     </div>
+<style>
+    .card {
+        margin: 1.5vw;
+        width: 190px;
+        height: 254px;
+        border-radius: 30px;
+        background: #e0e0e0;
+        box-shadow: 15px 15px 30px #bebebe,
+        -15px -15px 30px #ffffff;
+    }
+</style>
